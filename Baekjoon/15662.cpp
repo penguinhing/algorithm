01cf17b34e041b809a4f6bfa7234d2357a827f03
펭귄 <69632413+penguinhing@users.noreply.github.com>
2024-09-12 01:52:16 +0900

    Create 15662.cpp

---
+++ Baekjoon/15662.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int N, K, a, b, result = 0, npos, arrow;
+string cog[1004], tmp;
+
+void rotate(int number, int direction) {
+	queue<pair<int, int>> q;
+	int visited[1004] = { 0 };
+	visited[number] = 1;
+	q.push({number, direction});
+	while(!q.empty()) {
+		npos = q.front().first;
+		arrow = q.front().second; q.pop();
+		
+		if(npos-1 >= 0 && visited[npos-1] == 0 && cog[npos-1][2] != cog[npos][6]) {
+			q.push({npos-1, arrow * -1});
+			visited[npos-1] = 1;
+		}
+		if(npos+1 < N && visited[npos+1] == 0 && cog[npos+1][6] != cog[npos][2]) {
+			q.push({npos+1, arrow * -1});
+			visited[npos+1] = 1;
+		}
+		
+		tmp = "";
+		if(arrow == 1) {
+			tmp += cog[npos][cog[npos].length()-1];
+			for(int i = 0; i < cog[npos].length()-1; ++i) tmp += cog[npos][i];
+		}
+		else {
+			for(int i = 1; i < cog[npos].length(); ++i) tmp += cog[npos][i];
+			tmp += cog[npos][0];
+		}
+		cog[npos] = tmp;
+	}
+}
+
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(NULL);
+	cin >> N;
+	for(int i = 0; i < N; ++i) cin >> cog[i];
+		
+	cin >> K;
+	for(int i = 0; i < K; ++i) {
+		cin >> a >> b;
+		rotate(a-1, b);
+	}
+	for(int i = 0; i < N; ++i) result += cog[i][0] - '0';
+	
+	cout << result;
+}
