f28b521839a0bb486c63c6b1e5ea361d687a163b
펭귄 <69632413+penguinhing@users.noreply.github.com>
2024-09-26 02:58:41 +0900

    Create 1927.cpp

---
+++ Baekjoon/1927.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int N, a;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(NULL);
+	cin >> N;
+	for (int i = 0; i < N; ++i) {
+		cin >> a;
+		if (a == 0) {
+			cout << (pq.empty() ? 0 : pq.top()) << "\n";
+			if (!pq.empty()) pq.pop();
+		}
+		else pq.push(a);
+	}
+}
