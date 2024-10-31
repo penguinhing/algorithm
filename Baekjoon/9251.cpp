01b8ff408ae1fcf138ba7ea11d010712aea00fa8
펭귄 <69632413+penguinhing@users.noreply.github.com>
2024-10-31 23:45:51 +0900

    Create 9251.cpp

---
+++ Baekjoon/9251.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+string s1, s2;
+int dp[1004][1004], result = 0;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(NULL);
+	cin >> s1 >> s2;
+	for (int i = 1; i <= s1.length(); ++i) {
+		for (int j = 1; j <= s2.length(); ++j) {
+			if (s1[i - 1] == s2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+			else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			result = max(dp[i][j], result);
+		}
+	}
+	cout << result;
+}
